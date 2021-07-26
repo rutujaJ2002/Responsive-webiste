294b7d4e527dbd3e92b632e73e9fb582462d6b75
rutujaJ2002 <82172257+rutujaJ2002@users.noreply.github.com>
2021-07-26 20:07:23 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const logo=document.getElementById('text');
+const typewriter= new Typewriter(text,{
+
+    strings:["I Love <bold class='primary'>Coding</bold>", "And <bold class='primary'>sharing</bold> it with you!"],
+
+    autoStart:true,
+        loop: true,
+        delay: 20,
+
+});
